3695ee6b6c196540e7737cf5053bd406844ad451
Ivan Molto <ivanmolto@gmail.com>
2023-03-15 23:18:55 +0100

    chore: Add Index routes

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Root, {
 import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
