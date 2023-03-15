07c2d9a5205cd9a5954acb7b52a8a01bccaffc7f
Ivan Molto <ivanmolto@gmail.com>
2023-03-15 22:59:15 +0100

    chore: Add Global pending UI

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,10 +1,10 @@
 import {
   Form,
-  Link,
   NavLink,
   Outlet,
-  useLoaderData,
   redirect,
+  useLoaderData,
+  useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
@@ -20,6 +20,7 @@ export async function loader() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
   return (
     <>
       <div id="sidebar">
@@ -70,7 +71,10 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div
+        id="detail"
+        className={navigation.state === "loading" ? "loading" : ""}
+      >
         <Outlet />
       </div>
     </>
